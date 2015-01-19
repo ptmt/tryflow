1482ae7bf0e6b16ccacdd502a77d5c27ef5b1dac
Dmitry unknownexception <unknownliveid@hotmail.com>
2015-01-19 10:21:21 +0600

    update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: start.sh
+forever: npm install forever -g
+libelf: apt-get install -y libelf-dev
+flowbin: npm install flow-bin -g
+web: forever start app/server.compiled/index.js
--- start.sh
+++
@@ -1,6 +0,0 @@
-npm install forever -g
-# https://github.com/facebook/flow/issues/210
-apt-get install -y libelf-dev
-export USER=u20848
-npm install flow-bin -g
-forever start app/server.compiled/index.js
