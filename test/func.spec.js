fbb86c589eafb5e33ee274c940e2df8e455c34f3
Dmitry unknownexception <unknownliveid@hotmail.com>
2015-02-22 12:42:14 +0600

    fix tests: add regex to test version

--- test/func.spec.js
+++ test/func.spec.js
@@ -4,7 +4,8 @@ var check = require('../app/server.compiled/flow_check');
 describe('Version', function(){
   it('should return version', function(done){
     check.version(function(err, version) {
-      assert.equal(version.indexOf('version') > -1, true);
+      var ver = new RegExp("\d*\.\d*\.\d*")
+      assert.equal(ver.test(version), true);
       done();
     })
   })
